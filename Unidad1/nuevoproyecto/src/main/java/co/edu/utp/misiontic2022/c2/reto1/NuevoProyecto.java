ab7a3fdddc32a7df2fcad48ecddb446969e6e00d
marr4631 <mauricio.rodriguez@christus.co>
2021-07-12 17:11:11 -0500

    Java project made to solve reto1 from ciclo2

---
+++ Unidad1/nuevoproyecto/src/main/java/co/edu/utp/misiontic2022/c2/reto1/NuevoProyecto.java
@@ -0,0 +1,48 @@
+package co.edu.utp.misiontic2022.c2.reto1;
+
+public class NuevoProyecto {
+    public static void main(String[] args) {
+            //Instación de clase y uso de métodos
+    }
+
+    private int periodo;
+    private Double monto;
+    private Double interes;
+    
+    // -----------------------------------------------------------------
+// Atributos
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// Métodos
+// -----------------------------------------------------------------
+    NuevoProyecto(){
+        this(0, 0.0, 0.0);
+    }
+
+    NuevoProyecto(int periodo, Double monto, Double interes){
+        this.periodo = periodo;
+        this.monto = monto;
+        this.interes = interes;
+    }
+
+    public void calcularInteresSimple(){
+
+    }
+    public void calacularInteresCompuesto(){
+
+    }
+    /**
+    * Método para comparar la diferencia en el total de intereses generados para
+    el proyecto.
+    * @return Respuesta al Reto.
+    */
+    public String compararInversion ( int pPeriodo, double pMonto, double pInteres )
+    {
+    }
+    public double compararInversion ( )
+    {
+    //codigo
+    }
+    
+}
+
